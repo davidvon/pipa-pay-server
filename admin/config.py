a5d7f46b7dcfab1f10a15360bdf5616bba6216cb
davidvon <davidvon71@163.com>
2016-10-23 12:52:40 +0800

    车享惠服务器配置参数

--- admin/config.py
+++ admin/config.py
@@ -7,45 +7,43 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 LANGUAGES = {'zh_CN': 'Chinese'}
 BABEL_DEFAULT_LOCALE = 'zh_CN'
 
-# weixin account
-ISONLINE = 'ONLINE_SERVER' in os.environ
+ISONLINE = 'PIPAPAY_ONLINE_SERVER' in os.environ
+
 WEIXIN_TOKEN = 'pipapay'
 WXPAY_CONIFG = {
-    'partnerId': '1304148101',
-    'partnerKey': '',
-    'apiKey': '85a3027dez49d259pipapay26fr58yJH'
+    'mch_id': '1220396801',
+    'api_key': 'FuiNBoblzcPN9MXq6nFxRnqcwyqf81vc',
+    'partnerKey': ''  # v2
 }
 
-RUN_MODE = os.environ.get('PIPA_RUN_MODE', 'debug')
-
+RUN_MODE = os.environ.get('PIPAPAY_RUN_MODE', 'debug')
 if RUN_MODE == 'production':
     DEBUG = False
-    DEFAULT_HOST_URL = 'http://wx.pipapay.com/'
-    SQLALCHEMY_DATABASE_URI = 'mysql://pipa_b:pidb75P23aPavp@rds4w9icyicw33o374fc.mysql.rds.aliyuncs.com/pipa?unix_socket=/tmp/mysql.sock&charset=utf8'
-    REDIS_SERVER_IP = '2e57dec587ee4fd5.m.cnsza.kvstore.aliyuncs.com'
-    REDIS_SERVER_PWD = 'piPay75P23aPavp'
+    DEFAULT_HOST_URL = 'http://pay.chexianghui.com/'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:Davidvon12345@127.0.0.1:33006/pipapay?unix_socket=/tmp/mysql.sock&charset=utf8'
+    REDIS_SERVER_IP = '127.0.0.1'
+    REDIS_SERVER_PWD = ''
     REDIS_SERVER_DB = 1
-    WEIXIN_APPID = 'wx6965cc85ec3e801c'
-    WEIXIN_SECRET = '7fb5fff43e61aad157aa107aa8301eb7'
+    WEIXIN_APPID = 'wxacfa25edbc32b183'
+    WEIXIN_SECRET = 'c780474a8dc84887e1b55cd18ef3031d'
 elif RUN_MODE == 'sandbox':
     DEBUG = True
-    DEFAULT_HOST_URL = 'http://wx.pipapay.com/'
-    SQLALCHEMY_DATABASE_URI = 'mysql://pipa_b:pidb75P23aPavp@rds4w9icyicw33o374fc.mysql.rds.aliyuncs.com/pipa?unix_socket=/tmp/mysql.sock&charset=utf8'
-    REDIS_SERVER_IP = '2e57dec587ee4fd5.m.cnsza.kvstore.aliyuncs.com'
-    REDIS_SERVER_PWD = 'piPay75P23aPavp'
+    DEFAULT_HOST_URL = 'http://pay.chexianghui.com/'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:Davidvon12345@127.0.0.1:33006/pipapay?unix_socket=/tmp/mysql.sock&charset=utf8'
+    REDIS_SERVER_IP = '127.0.0.1'
+    REDIS_SERVER_PWD = ''
     REDIS_SERVER_DB = 1
     WEIXIN_APPID = 'wxb3ec764893b99722'
     WEIXIN_SECRET = '04a37bc738a0c2759ba850c4334b99fc'
 else:
     DEBUG = True
     DEFAULT_HOST_URL = 'http://127.0.0.1/'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Davidvon12345@127.0.0.1/pipapay?unix_socket=/tmp/mysql.sock&charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:Davidvon12345@127.0.0.1:33006/pipapay?unix_socket=/tmp/mysql.sock&charset=utf8'
     REDIS_SERVER_IP = '127.0.0.1'
     REDIS_SERVER_PWD = ''
     REDIS_SERVER_DB = 1
     WEIXIN_APPID = 'wxb3ec764893b99722'
     WEIXIN_SECRET = '04a37bc738a0c2759ba850c4334b99fc'
-
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
@@ -57,4 +55,3 @@ LOG_DIR = os.path.join(basedir, "../logs")
 LOG_FILE = os.path.join(LOG_DIR, "server.log")
 if not os.path.exists(LOG_DIR):
     os.mkdir(LOG_DIR)
-
